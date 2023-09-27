f58df89a6e2afbb1262abbb5246cb3cbc7936876
dylancedres <70666980+dylancedres@users.noreply.github.com>
2023-09-27 08:40:31 -0400

    Update a.py

--- a.py
+++ a.py
@@ -127,7 +127,7 @@ logo, title = st.columns([0.34, 0.66])
     # st.title("Dashboard: Social Determinants of Health and Kidney Disease Lab Data")
     # st.header("Dashboard: Social Determinants of Health and Kidney Disease Lab Data")
 with logo:
-    st.image("logo.png", width=200)
+    st.image("logo.jpeg", use_column_width="never")
     
 with title:
     # Application's Main Title (CENTER)
