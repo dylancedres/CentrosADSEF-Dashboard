97fc6d83de209b7e0ca7c00af53526c7463cced0
dylancedres <70666980+dylancedres@users.noreply.github.com>
2023-10-05 18:19:53 -0400

    Update a.py

--- a.py
+++ a.py
@@ -445,6 +445,8 @@ st.markdown(body="")
 
 
 # Resources Information Paragraph
+footbuff1, footnote, footbuff2 = st.columns([0.10, 0.80, 0.10], gap="small")
+
 # st.markdown("""<p style="text-align:center; background-color:white; font-size:12px; color:black">
 #                 SDoHs were obtained from Agency for Healthcare Research and Quality (AHRQ) database (https://www.ahrq.gov/).<br><br>
 #                 Kidney-related lab test results were obtained as part of the pilot project supported by the Office of the Director,<br>
@@ -455,15 +457,16 @@ st.markdown(body="")
 #                 The work is solely the authors' responsibility and does not necessarily represent the official view of the National Institutes of Health.
 #             </p>""", unsafe_allow_html=True)
 
-st.caption("""<p style="text-align:center;">
-                SDoHs were obtained from Agency for Healthcare Research and Quality (AHRQ) database (https://www.ahrq.gov/).<br><br>
-                Kidney-related lab test results were obtained as part of the pilot project supported by the Office of the Director,<br>
-                National Institutes of Health Common Fund under award number 1OT2OD032581-01<br>
-                (Artificial Intelligence/Machine Learning Consortium to Advance Health Equity and Researcher Diversity (AIM-AHEAD)).<br><br> 
-                It was also supported by the Center for Collaborative Research in Health Disparities (CCRHD),<br>
-                RCMI grant U54 MD007600 (National Institute on Minority Health and Health Disparities) from the National Institutes of Health.<br><br> 
-                The work is solely the authors' responsibility and does not necessarily represent the official view of the National Institutes of Health.
-           </p>""", unsafe_allow_html=True)
+with footnote:
+    st.caption("""<p style="text-align:center;">
+                   SDoHs were obtained from Agency for Healthcare Research and Quality (AHRQ) database (https://www.ahrq.gov/).
+                   Kidney-related lab test results were obtained as part of the pilot project supported by the Office of the Director,
+                   National Institutes of Health Common Fund under award number 1OT2OD032581-01
+                   (Artificial Intelligence/Machine Learning Consortium to Advance Health Equity and Researcher Diversity (AIM-AHEAD)).
+                   It was also supported by the Center for Collaborative Research in Health Disparities (CCRHD),
+                   RCMI grant U54 MD007600 (National Institute on Minority Health and Health Disparities) from the National Institutes of Health.
+                   The work is solely the authors' responsibility and does not necessarily represent the official view of the National Institutes of Health.
+               </p>""", unsafe_allow_html=True)
 
 st.caption("""SDoHs were obtained from Agency for Healthcare Research and Quality (AHRQ) database (https://www.ahrq.gov/).
               Kidney-related lab test results were obtained as part of the pilot project supported by the Office of the Director,
