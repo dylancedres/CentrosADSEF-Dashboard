1d9b5120e5bed71dbe07065c14555058f1682bdd
dylancedres <70666980+dylancedres@users.noreply.github.com>
2023-10-06 02:50:24 -0400

    Update a.py

--- a.py
+++ a.py
@@ -116,7 +116,7 @@ sdoh = load_data()      # SDOHs and Labs Data
 
 
 st.markdown("""<style>
-                  [data-testid=stHorizontalBlock]:nth-of-type(3){
+                  [data-testid=stHorizontalBlock]:nth-of-type(5){
                     gap: 0rem;
                   }
             </style>""", unsafe_allow_html=True)
