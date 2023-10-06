0bda4c669628410b3e9d93b9e0d922a39ab0b611
dylancedres <70666980+dylancedres@users.noreply.github.com>
2023-10-06 05:55:32 -0400

    Update a.py

--- a.py
+++ a.py
@@ -124,7 +124,7 @@ st.markdown("""<style>
 
 
 ### Dashboard Main Title ###
-logo1, title, logo2 = st.columns([0.13, 0.77, 0.10])
+logo1, title, logo2 = st.columns([0.12, 0.78, 0.10])
 # logo1, title, logo2 = st.columns([0.15, 0.10, 0.75])
 # logo, buff, title = st.columns([0.15, 0.10, 0.75])
 
@@ -151,7 +151,7 @@ with logo1:
 
 with title:
     # Application's Main Title (CENTER)
-    st.markdown("""<p style="text-align:center; font-weight:bold; font-size:36px">
+    st.markdown("""<p style="text-align:center; font-weight:bold; font-size:34px">
                     <br>Kidney Disease Lab Test / Social Determinants of Health (SDoH) - Puerto Rico</p>
                 """, unsafe_allow_html=True)
     # st.subheader("**Kidney Disease Lab Test / Social Determinants of Health (SDoH) - Puerto Rico**", anchor=False)
